3f1ab38c24ca41ae00e4ee17414b9e2e7cd90541
MariaShebeko <marygro89@gmail.com>
2023-03-20 16:24:42 +0100

    added about-page with cube animation

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,7 +1,77 @@
 import './AboutPage.scss'
+import Loader from 'react-loaders'
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { AnimatedLetters } from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCss3,
+  faGit,
+  faHtml5,
+  faJsSquare,
+  faNodeJs,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 export const AboutPage = () => {
-  return <div>AboutPage</div>
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className='container about-page'>
+        <div className='text-wrapper'>
+          <h1>
+            <AnimatedLetters
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+          <p>
+            I'm front-end developer looking for a role in IT company with the
+            opportunity to work with the latest technologies on challenging and
+            diverse projects.
+          </p>
+          <p>
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my skills.
+          </p>
+          <p>
+            I enjoy developing web applications or web sites. My stack of
+            technologies: HTML, CSS, JavaScript, React, Node.js.
+          </p>
+        </div>
+
+        <div className='cube-wrapper'>
+          <div className='cube-spinner'>
+            <div className='icon1'>
+              <FontAwesomeIcon icon={faReact} color='#5Ed4F4' />
+            </div>
+            <div className='icon2'>
+              <FontAwesomeIcon icon={faHtml5} color='#f06529' />
+            </div>
+            <div className='icon3'>
+              <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+            </div>
+            <div className='icon4'>
+              <FontAwesomeIcon icon={faJsSquare} color='#efd81d' />
+            </div>
+            <div className='icon5'>
+              <FontAwesomeIcon icon={faGit} color='#ec4d28' />
+            </div>
+            <div className='icon6'>
+              <FontAwesomeIcon icon={faNodeJs} color='#43853d' />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type='pacman' />
+    </>
+  )
 }
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,6 +4,7 @@ import letterM from '../../assets/images/letterM.png'
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../config'
 import { AnimatedLetters } from '../AnimatedLetters'
+import Loader from 'react-loaders'
 
 export const HomePage = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -36,32 +37,35 @@ export const HomePage = () => {
   }, [])
 
   return (
-    <div className='container home-page'>
-      <div className='text-wrapper'>
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={letterM} alt='name' />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={workArray}
-            idx={22}
-          />
-        </h1>
-        <h2> (JavaScript, React)</h2>
-        <Link to={ROUTES.contact} className='contact-btn'>
-          CONTACT ME
-        </Link>
+    <>
+      <div className='container home-page'>
+        <div className='text-wrapper'>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={letterM} alt='name' />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={workArray}
+              idx={22}
+            />
+          </h1>
+          <h2> (JavaScript, React)</h2>
+          <Link to={ROUTES.contact} className='contact-btn'>
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type='pacman' />
+    </>
   )
 }
